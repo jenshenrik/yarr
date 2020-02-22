3d7bfe1b93a9e1a7865011dfd203289df4bcf5c2
Jens Henrik Vogelius <jens.vogelius@isobar.com>
2020-02-23 00:14:43 +0100

    Created generic entity, cleaned up rendering

---
+++ entity.py
@@ -0,0 +1,13 @@
+class Entity:
+    """
+    A generic object to represent players, enemies, items, etc
+    """
+    def __init__(self, x, y, char, colour):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.colour = colour
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
