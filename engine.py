02eb1eaa5a00f84d092cc928e5e27eb87587c6c2
Jens Henrik Vogelius <jens.vogelius@isobar.com>
2020-02-23 08:45:18 +0100

    Created rooms

--- engine.py
+++ engine.py
@@ -19,6 +19,7 @@ def main():
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
     entities = [npc, player]
     game_map = GameMap(map_width, map_height)
+    game_map.make_map()
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
