6b8cede5ea6d62d01828063a3f401d78e3fa6a28
Jens Henrik Vogelius <jens.vogelius@isobar.com>
2020-07-08 14:40:28 +0200

    Created heal function for potion and implemented consuming items

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -4,6 +4,7 @@ from random import randint
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
 from entity import Entity
+from item_functions import heal
 from render_functions import RenderOrder
 from components.ai import BasicMonster
 from components.fighter import Fighter
@@ -120,6 +121,6 @@ class GameMap:
             y = randint(room.y1 + 1, room.y2 - 1)
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-                item_component = Item()
+                item_component = Item(use_function=heal, amount=4)
                 item = Entity(x, y, '!', tcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM, item=item_component)
                 entities.append(item)
