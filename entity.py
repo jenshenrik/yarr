1be9611314981d3776a19a411455fca3fdc6026a
Jens Henrik Vogelius <jens.vogelius@isobar.com>
2020-07-10 13:12:34 +0200

    Created equippables

--- entity.py
+++ entity.py
@@ -1,6 +1,7 @@
 import math
 import tcod
 
+from components.item import Item
 from render_functions import RenderOrder
 
 class Entity:
@@ -9,7 +10,7 @@ class Entity:
     """
     def __init__(self, x, y, char, colour, name, blocks = False, 
             render_order=RenderOrder.CORPSE, fighter=None, ai=None, item=None, 
-            inventory=None, stairs=None, level=None):
+            inventory=None, stairs=None, level=None, equipment=None, equippable=None):
         self.x = x
         self.y = y
         self.char = char
@@ -23,6 +24,8 @@ class Entity:
         self.inventory = inventory
         self.stairs = stairs
         self.level = level
+        self.equipment = equipment
+        self.equippable = equippable
 
         if self.fighter:
             self.fighter.owner = self
@@ -41,6 +44,17 @@ class Entity:
 
         if self.level:
             self.level.owner = self
+
+        if self.equipment:
+            self.equipment.owner = self
+
+        if self.equippable:
+            self.equippable.owner = self
+
+            if not self.item:
+                item = Item()
+                self.item = item
+                self.item.owner = self
 
     def move(self, dx, dy):
         self.x += dx
